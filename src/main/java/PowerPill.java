6cc5343ec559e23cc7b1d350c9809b589f45fe62
IsataGeorge <156387963+IsataGeorge@users.noreply.github.com>
2024-02-11 21:25:28 -0500

    Completed

--- src/main/java/PowerPill.java
+++ src/main/java/PowerPill.java
@@ -9,30 +9,81 @@
  */
 public class PowerPill
 {
-    // TODO - replace this line with instruction from step 1
-
+    private final static int DEFAULT_POWER = 10;
     // instance variables
-    // TODO - replace this line with instruction from step 2
-    // TODO - replace this line with instruction from step 3
+    int power = DEFAULT_POWER;
+    String name;
 
     // constructors
 
-    // TODO - replace this line with instruction from step 4
+    /**
+     \* Initializes this power pill to a default power value
+     \* and sets the name of the pill to name.
+     \* @param name the name of this power pill.
+     */
+    public PowerPill(String name)
+    {
+        this.name = name;
+    }
 
-    // TODO - replace this line with instruction from step 5
-
+    /**
+     * Initializes this power pill to the value of power
+     * and sets the name of the pill to name.
+     * @param name the name of this power pill
+     * @param power the power level of this power pill.
+     */
+    public PowerPill(String name, int power)
+    {
+        this.name = name;
+        this.power = power;
+    }
 
     // accessor methods
 
-    // TODO - replace this line with instruction from step 6
+    /**
+     *
+     * @return the name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     *
+     * @return the power value
+     */
+    public int getPower()
+    {
+        return power;
+    }
 
 
     // mutator methods
 
-    // TODO - replace this line with instruction from step 7
+    /**
+     *
+     * @param name sets the name
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @param power sets the power
+     */
+    public void setPower(int power)
+    {
+        this.power = power;
+    }
 
     // toString method
 
-    // TODO - replace this line with instruction from step 8
+    public String toString()
+    {
+        return ("PowerPill " + name + " = " + power);
+    }
 
 }
